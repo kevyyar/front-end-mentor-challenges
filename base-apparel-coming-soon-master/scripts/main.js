a84680eb863dd9642ef01cd4e6dd93e4a869c9c1
Kevin Barreto <barretokevin@hotmail.com>
2020-03-21 08:43:23 -0600

    updated the script

--- base-apparel-coming-soon-master/scripts/main.js
+++ base-apparel-coming-soon-master/scripts/main.js
@@ -1,23 +1,22 @@
-const form = document.getElementById('form')
-const email = document.getElementById('email')
+const form = document.getElementById("form");
+const email = document.getElementById("email");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const emailVal = email.value 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const emailVal = email.value;
 
-    // check if it's valid email
-    if(!validateEmail(emailVal)) {
-        form.classList.add('error')
-        form.classList.remove('success')
-    } else {
-        form.classList.remove('error')
-        // form.classList.add('success')
-        document.body.innerHTML = 'Thank you'
-    }
-
-})
+  // check if it's valid email
+  if (!validateEmail(emailVal)) {
+    form.classList.add("error");
+    form.classList.remove("success");
+  } else {
+    form.classList.remove("error");
+    // form.classList.add('success')
+    document.body.innerHTML = `<div style="display: flex; align-items: center; justify-content: center;"><h1>Thank you</h1></div>`;
+  }
+});
 
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
